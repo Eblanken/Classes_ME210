e19ba752c815a3e6fdfcaf14abd4fe3df697a10b
Scott Blankenberg <sblankenl@stanford.edu>
2019-03-03 16:46:49 -0800

    Updated phototransistor code, duty cycle measure

--- Code/Test_Code_Scott/ME_210_PHOTOTRANSISTOR/src/main.cpp
+++ Code/Test_Code_Scott/ME_210_PHOTOTRANSISTOR/src/main.cpp
@@ -14,6 +14,15 @@ void determine_frequency_2(void);
 int count_1 = 0;
 int count_2 = 0; 
 
+unsigned long rising_edge_1_start = 0;
+unsigned long time_high_1 = 0;
+unsigned long rising_edge_2_start = 0;
+unsigned long time_high_2 = 0;
+
+unsigned long avg_duty_cycle_1 = 0;
+unsigned long avg_duty_cycle_2 = 0;
+
+
 bool freq_1_detected = false;
 bool freq_2_detected = false;
 
@@ -37,9 +46,9 @@ void setup() {
   //time = freq_conversion(frequency);
   //outputTimer.begin(write_output, time);
   frequencyTimer_1.begin(determine_frequency_1, 100000);
-  attachInterrupt(digitalPinToInterrupt(PIN_SIGNAL_IN), count_falling_edges_1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_SIGNAL_IN), count_falling_edges_1, CHANGE);
   frequencyTimer_2.begin(determine_frequency_2, 100000);
-  attachInterrupt(digitalPinToInterrupt(PIN_SIGNAL_IN_2), count_falling_edges_2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_SIGNAL_IN_2), count_falling_edges_2, CHANGE);
 }
 
 // Updates frequency of signal depending on potentiometer wiper value
@@ -76,13 +85,44 @@ void loop() {
 }
 
 
+
 // Counts edges by incrementing on each call
 void count_falling_edges_1(void) {
-  count_1 = count_1+1;
+
+  bool value = digitalRead(PIN_SIGNAL_IN);
+  if(value == 0) {
+    //falling edge
+    count_1 = count_1+1;
+    unsigned long curr_time = micros();
+    time_high_1 = curr_time - rising_edge_1_start;
+    avg_duty_cycle_1 = avg_duty_cycle_1 + time_high_1;
+  }
+  else {
+    //rising edge
+    rising_edge_1_start = micros();
+  }
+  
+
+  
 }
 
 void count_falling_edges_2(void) {
-  count_2 = count_2+1;
+
+  bool value = digitalRead(PIN_SIGNAL_IN_2);
+  if(value == 0) {
+    //falling edge
+    count_2 = count_2+1;
+    unsigned long curr_time = micros();
+    time_high_2 = curr_time - rising_edge_2_start;
+    avg_duty_cycle_2 = avg_duty_cycle_2 + time_high_2;
+
+  }
+  else {
+    //rising edge
+    rising_edge_2_start = micros();
+  }
+  
+
 }
 
 
@@ -91,12 +131,17 @@ void determine_frequency_1(void) {
   detachInterrupt(PIN_SIGNAL_IN);
   if (count_1 >= 80 && count_1 <= 200) {
     freq_1_detected = true;
-    Serial.println(count_1);
+    Serial.print("freq_1 = ");
+    Serial.println(10*count_1);
+    unsigned long reported_duty_cycle_1 = (100*avg_duty_cycle_1)/100000;
+    Serial.print("duty_cycle_1 = ");
+    Serial.println(reported_duty_cycle_1);
   } else {
     freq_1_detected = false;
   }
+  avg_duty_cycle_1 = 0;
   count_1 = 0;
-  attachInterrupt(digitalPinToInterrupt(PIN_SIGNAL_IN), count_falling_edges_1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_SIGNAL_IN), count_falling_edges_1, CHANGE);
 }
 
 // Calls Interrupt to Determine Number of Edges
@@ -104,12 +149,18 @@ void determine_frequency_2(void) {
   detachInterrupt(PIN_SIGNAL_IN_2);
   if (count_2 >= 80 && count_2 <= 200) {
     freq_2_detected = true;
-    Serial.println(count_2);
+    Serial.print("freq_2 = ");
+    Serial.println(10*count_2);
+    unsigned long reported_duty_cycle_2 = (100*avg_duty_cycle_2)/100000;
+    Serial.print("duty_cycle_2 = ");
+    Serial.println(reported_duty_cycle_2);
   } else {
     freq_2_detected = false;
+
   }
+  avg_duty_cycle_2 = 0;
   count_2 = 0;
-  attachInterrupt(digitalPinToInterrupt(PIN_SIGNAL_IN_2), count_falling_edges_2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_SIGNAL_IN_2), count_falling_edges_2, CHANGE);
 }
 
 uint8_t TestForKey(void) {
